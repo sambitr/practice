aa9ec2b90e57b13370d0f928d5127391ecea680c
skrout <skrout@deloitte.co.uk>
2025-09-26 09:15:25 +0100

    updated test code

--- test/test_two_sum.py
+++ test/test_two_sum.py
@@ -1,9 +1,17 @@
 import pytest
 from src.two_sum import two_sum
 
-@pytest.mark.parametrize("a,b,expected", [(2,3,5), (-1,1,0), (0,0,0)])
-def test_sum_param(a, b, expected):
-    assert two_sum(a, b) == expected
+# @pytest.mark.parametrize("a,b,expected", [(2,3,5), (-1,1,0), (0,0,0)])
+# def test_sum_int(a, b, expected):
+#     assert two_sum(a, b) == expected
+def test_sum_int():
+    assert two_sum(2, 3) == 5
+    assert two_sum(-1, 1) == 0
+    assert two_sum(0, 0) == 0
+
+def test_sum_str():
+    assert two_sum("a", "A") == "aA"
+    assert two_sum("Hello, ", "World!") == "Hello, World!"
 
 def test_type_error():
     with pytest.raises(TypeError):
